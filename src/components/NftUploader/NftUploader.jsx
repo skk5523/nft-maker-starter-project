062805d2e76a4654380d46bccd1e8392e8192368
skk5523a <skk5523.a@gmail.com>
2022-11-12 19:32:28 +0900

    Upgrade the app with loading animations

--- src/components/NftUploader/NftUploader.jsx
+++ src/components/NftUploader/NftUploader.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import { useEffect, useState } from 'react'
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
+import Animation from "../Animation/Animation";
 // require('dotenv').config();
 
 // Import the NFTStorage class and File constructor from the 'nft.storage' package
@@ -19,8 +20,13 @@ const NftUploader = () => {
    * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
    */
   const [currentAccount, setCurrentAccount] = useState("");
+  const [isOnGoerli, setIsOnGoerli] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isImageReceived, setIsImageReceived] = useState(false);
+  const [isMintingInProgress, setIsMintingInProgress] = useState(false);
+
   /*この段階でcurrentAccountの中身は空*/
-  console.log("currentAccount: ", currentAccount);
+  // console.log("currentAccount: ", currentAccount);
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
     if (!ethereum) {
@@ -36,10 +42,18 @@ const NftUploader = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+      // 0x5 は Goerli の ID です。
+      const goerliChainId = "0x5";
+      setIsOnGoerli(chainId === goerliChainId);
+      
     } else {
       console.log("No authorized account found");
     }
   };
+
   const connectWallet = async () =>{
     try {
       const { ethereum } = window;
@@ -62,10 +76,29 @@ const NftUploader = () => {
       console.log(error);
     }
   };
+  
+  const checkChainId = async () =>{
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+      // 0x5 は Goerli の ID です。
+      const goerliChainId = "0x5";
+      setIsOnGoerli(chainId === goerliChainId);
+      
+    } catch (error) {
+      console.log(error);
+    }  
+  };  
 
   const askContractToMintNft = async (ipfs) => {
     const CONTRACT_ADDRESS =
-      "0xccDb12DDadcF8218c4dda0beeD960985eA21CA8E";
+    "0xccDb12DDadcF8218c4dda0beeD960985eA21CA8E";
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -78,11 +111,14 @@ const NftUploader = () => {
         );
         console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.mintIpfsNFT("sample",ipfs);
+        setIsMintingInProgress(true);
         console.log("Mining...please wait.");
         await nftTxn.wait();
-        console.log(
+        setIsMintingInProgress(false);
+        alert(
           `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
         );
+        setIsImageReceived(false);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -92,11 +128,21 @@ const NftUploader = () => {
   }; 
   
   const renderNotConnectedContainer = () => (
-    <button onClick={connectWallet} className="cta-button connect-wallet-button">
-        Connect to Wallet
-      </button>
-    );
-    /*
+    <button onClick={connectWallet} className="connectWalletButton">
+      Connect to Wallet
+    </button>
+  );
+  
+  const renderPrePage = () => (
+    <>
+      {currentAccount === "" && renderNotConnectedContainer()}
+      {currentAccount && !isOnGoerli && (
+          <p>Please switch to the Goerli network!</p>
+      )}
+    </>    
+  );
+
+   /*
     * ページがロードされたときに useEffect()内の関数が呼び出されます。
     */
    useEffect(() => {
@@ -104,13 +150,17 @@ const NftUploader = () => {
     }, []);
   
   const imageToNFT = async (e) => {
+    setIsImageReceived(true);
     const images = e.target.files;
     // create a new NFTStorage client using our API key
     const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
     
     for (const image of images) {
       // call client.store, passing in the image & metadata
+      setIsUploading(true);
       const cid = await nftstorage.storeBlob(image);
+      setIsUploading(false);
+
       console.log(cid);
       askContractToMintNft(cid);
     }
@@ -118,26 +168,36 @@ const NftUploader = () => {
 
   return (
     <div className="outerBox">
-      {currentAccount === "" ? (
-        renderNotConnectedContainer()
-      ) : (
-        <p>If you choose image, you can mint your NFT</p>
-      )}
       <div className="title">
         <h2>NFTアップローダー</h2>
       </div>
-      <div className="nftUplodeBox">
-        <div className="imageLogoAndText">
-          <img src={ImageLogo} alt="imagelogo" />
-          <p>ここにドラッグ＆ドロップしてね</p>
-        </div>
-        <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png" onChange={imageToNFT}  />
-      </div>
-      <p>または</p>
-      <Button variant="contained">
-        ファイルを選択
-        <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png" onChange={imageToNFT} />
-      </Button>
+      {renderPrePage()}
+      {currentAccount && isOnGoerli && !isImageReceived && !isUploading && (
+        <>
+          <p>If you choose image, you can mint your NFT</p>
+          <div className="nftUplodeBox">
+            <div className="imageLogoAndText">
+              <img src={ImageLogo} alt="imagelogo" />
+              <p>ここにドラッグ＆ドロップしてね</p>
+            </div>
+            <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png" onChange={imageToNFT}  />
+          </div>
+          <p>または</p>
+          <Button variant="contained">
+            ファイルを選択
+            <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png" onChange={imageToNFT} />
+          </Button>
+        </>
+      )}
+      {currentAccount && isUploading && (
+        <Animation text="Uploading image..." />
+      )}
+      {currentAccount && !isUploading && isImageReceived && !isMintingInProgress && (
+        <p>Please confirm the transaction on your wallet!</p>
+      )}
+      {currentAccount && isMintingInProgress && (
+        <Animation text="Minting..." />
+      )}
     </div>
   );
 };
